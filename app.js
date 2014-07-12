81dbd0f1e1f46acb214104fb365c52f5c5c719d1
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2014-07-12 20:51:32 +0200

    sendgrid

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ var crypto = require('crypto');
 var TTL = 5 * 60;
 
 var Gpio = require('onoff').Gpio;
+var sendgrid  = require('sendgrid')('catena', 'catena01');
+
 
 var on = new Gpio(23,'out');
 var off = new Gpio(24,'out');
@@ -51,10 +53,34 @@ localSecureApp.get('/bon', security, function (req, res){
 		console.log('on depressed');
 	}, 1000);
 
+
+	sendgrid.send({
+		to:       'goofyahead@gmail.com',
+		from:     'usage@catena.org',
+		subject:  'Your device is in use',
+		text:     'The device is on use and you have received the payment for its use.'
+	}, function(err, json) {
+		console.log('email sent');
+		if (err) { 
+			return console.error(err);
+		}
+		console.log(json);
+	});
+
 	res.send('B on');
 });
 
 localSecureApp.get('/boff', security, function (req, res){
+
+	sendgrid.send({
+		to:       'goofyahead@gmail.com',
+		from:     'usage@catena.org',
+		subject:  'Your device is free again',
+		text:     'The device is ready for another user!'
+	}, function(err, json) {
+		if (err) { return console.error(err); }
+		console.log(json);
+	});
 
 	off.writeSync(1); 
 	console.log('off pressed');
